f36886a744cee478b9fc20c5c0ed90dc8bab4742
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-09-14 11:21:32 -0400

    updates

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-use RootsMagic\{FileReader, FileMover, MediaExtractor};
+use RootsMagic\{FileReader, FileMover, MediaCopier};
 
 include 'vendor/autoload.php';
 
@@ -8,7 +8,7 @@ locale_set_default('de_DE');
 
 //$ancestry_folder = "~/d/genealogy/roots-magic-09-06-2023_media/";
 
-$mediaExtractor = new MediaExtractor("~/d/genealogy/roots-magic-09-06-2023_media/",
+$mediaCopier = new MediaCopier("~/d/genealogy/roots-magic-09-06-2023_media/",
                          "~/temp/ancestry-media");
 
 $file = new FileReader('output.txt');
