980d3de474297cc71b181bdde2facdf1ac9010be
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-09-14 15:07:56 -0400

    $1

--- main.php
+++ main.php
@@ -8,7 +8,7 @@ locale_set_default('de_DE');
 
 //$ancestry_folder = "~/d/genealogy/roots-magic-09-06-2023_media/";
 
-$mediaCopier = new MediaCopier("/home/kurt/d/genealogy/roots-magic-09-06-2023_media/",
+$mediaCopier = new MediaCopier("/home/kurt/d/genealogy/roots-magic/sep-14-2023_media",
                          "/home/kurt/temp/ancestry-media");
 
 $file = new FileReader('output.txt');
